8fb1b487e8ca44fdf9fd68d55310f55206ede094
Drake Alia <52472741+DrakeAlia@users.noreply.github.com>
2024-11-05 12:32:12 -0800

    got the images to fit better in the trash can

--- src/app/components/photo-animation.tsx
+++ src/app/components/photo-animation.tsx
@@ -95,6 +95,7 @@ export function PhotoAnimation() {
                               fill="none"
                             >
                               <path
+                                className="bg-white"
                                 fillRule="evenodd"
                                 clipRule="evenodd"
                                 d="M12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2ZM15.5805 9.97493C15.8428 9.65434 15.7955 9.18183 15.4749 8.91953C15.1543 8.65724 14.6818 8.70449 14.4195 9.02507L10.4443 13.8837L9.03033 12.4697C8.73744 12.1768 8.26256 12.1768 7.96967 12.4697C7.67678 12.7626 7.67678 13.2374 7.96967 13.5303L9.96967 15.5303C10.1195 15.6802 10.3257 15.7596 10.5374 15.7491C10.749 15.7385 10.9463 15.6389 11.0805 15.4749L15.5805 9.97493Z"
@@ -126,7 +127,8 @@ export function PhotoAnimation() {
                         height={200}
                         width={200}
                       />
-                      <div className="absolute inset-0 bg-black/0 transition-colors duration-300 group-hover:bg-black/10" />
+                      <div className="absolute inset-0 bg-black/0 transition-colors duration-300 group-hover:bg-black/10" />{" "}
+                      Added hover overlay
                     </button>
                   </motion.li>
                 );
@@ -205,6 +207,7 @@ export function PhotoAnimation() {
             </motion.div>
           ) : null}
         </AnimatePresence>
+
         {readyToRemove ? (
           <motion.div
             initial={{ scale: 1.2, opacity: 0, filter: "blur(4px)" }}
@@ -226,6 +229,7 @@ export function PhotoAnimation() {
             </button>
           </motion.div>
         ) : null}
+
         <AnimatePresence>
           {readyToRemove ? (
             <div className="absolute top-1/2 z-10 h-[114px] w-24 -translate-y-1/2">
@@ -257,8 +261,8 @@ export function PhotoAnimation() {
                       alt="A guy"
                       className="rounded"
                       src={getImagePath(image)}
-                      height={65}
-                      width={65}
+                      height={59}
+                      width={59}
                       style={{
                         rotate:
                           index % 2 === 0
